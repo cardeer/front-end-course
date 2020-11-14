34134126ef6a5c3fd70c6f5d9e20c9ba74a452d7
CARDEER <cardeer123_1@hotmail.com>
2020-11-14 15:02:21 +0700

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,122 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="layout">
+    <div class="main">
+      <div class="products"></div>
+
+      <div class="orders">
+        <h2>Orders</h2>
+
+        <div class="order-list">
+          <div class="list" v-for="i in 100" :key="i">asdf {{ i }}</div>
+        </div>
+
+        <div class="total">
+          <p>Total</p>
+          <p>{{ total }}</p>
+        </div>
+
+        <div class="button-group">
+          <button class="btn" style="background-color: #5cb85c; color: white">
+            Checkout
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+<style scoped>
+.main {
+  display: flex;
+  flex-grow: 1;
+  max-height: 100vh;
 }
+
+.orders {
+  display: grid;
+  grid-template-rows: 50px 1fr 50px 50px;
+  gap: 10px;
+  /* align-items: center; */
+  margin-left: auto;
+  min-width: 300px;
+  padding: 10px;
+  background-color: rgb(228, 228, 228);
+  color: black;
+}
+
+.orders > h2 {
+  margin: 0;
+}
+
+.total {
+  display: flex;
+  font-weight: bold;
+  font-size: 20px;
+}
+
+.total > p {
+  margin: 0;
+}
+
+.total > p:last-child {
+  margin-left: auto;
+}
+
+.button-group {
+  display: flex;
+}
+
+.button-group > button {
+  flex-grow: 1;
+}
+
+.order-list {
+  overflow-y: auto;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+</style>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      total: 0,
+      orderList: [
+        {
+          name: "Lollipop",
+          quantity: 1,
+          pricePerUnit: 20,
+        },
+        {
+          name: "Snake",
+          quantity: 1,
+          pricePerUnit: 50,
+        },
+        {
+          name: "Cat",
+          quantity: 1,
+          pricePerUnit: 200,
+        },
+        {
+          name: "Dog",
+          quantity: 1,
+          pricePerUnit: 250,
+        },
+        {
+          name: "Computer",
+          quantity: 1,
+          pricePerUnit: 50000,
+        },
+        {
+          name: "Mouse",
+          quantity: 1,
+          pricePerUnit: 1379,
+        },
+      ],
+    };
+  },
+};
 </script>
